515d87243bf51340e4d1d6a1795c53edf2ad9510
joaopedroalb <joaopedroalb.dev@gmail.com>
2023-10-30 15:18:06 -0300

    test fetch cache no-store

--- src/app/api/anime/daily/route.js
+++ src/app/api/anime/daily/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const res = await fetch('https://back-anime-dle.onrender.com/daily')
+  const res = await fetch('https://back-anime-dle.onrender.com/daily', { cache: 'no-store' })
   const data = await res.json()
  
   return NextResponse.json({ data })
--- src/services/AnimeService.js
+++ src/services/AnimeService.js
@@ -1,7 +1,6 @@
 const getDailyAnime = async () => {
-  const res = await fetch(process.env.BASE_FETCH_URL + '/api/anime/daily')
+  const res = await fetch(process.env.BASE_FETCH_URL + '/api/anime/daily', { cache: 'no-store' })
   const { data } = await res.json()
-
   return data
 }
 
